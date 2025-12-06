ad9f4353cecae749840d33366ba9acb1ecc64732
Mohit <mk414822@gmail.com>
2025-12-06 16:18:16 +0530

    Second version of data saved

--- myCode.py
+++ myCode.py
@@ -9,9 +9,9 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# # Adding new row to df for v2
-# new_row_loc = {'Name': 'V2', 'Age': 20, 'City': 'City1'}
-# df.loc[len(df.index)] = new_row_loc
+# Adding new row to df for v2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 
 # # Adding new row to df for v3
